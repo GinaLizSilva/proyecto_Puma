49a51aeef83ef24b9b83521a8e54e8a731f39abd
Gina Liz Silva Condori <ginalizsilva@gmail.com>
2023-06-14 20:39:44 -0400

    proyecto

---
+++ Proy_Pumakatari/VenReportes.Designer.cs
@@ -0,0 +1,158 @@
+﻿namespace Proy_Pumakatari
+{
+    partial class VenReportes
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(VenReportes));
+            this.panel1 = new System.Windows.Forms.Panel();
+            this.iconButton1 = new FontAwesome.Sharp.IconButton();
+            this.cajaferroviaria = new FontAwesome.Sharp.IconButton();
+            this.iconButton2 = new FontAwesome.Sharp.IconButton();
+            this.label1 = new System.Windows.Forms.Label();
+            this.panel1.SuspendLayout();
+            this.SuspendLayout();
+            // 
+            // panel1
+            // 
+            this.panel1.BackColor = System.Drawing.Color.Transparent;
+            this.panel1.Controls.Add(this.iconButton1);
+            this.panel1.Controls.Add(this.cajaferroviaria);
+            this.panel1.Location = new System.Drawing.Point(12, 192);
+            this.panel1.Name = "panel1";
+            this.panel1.Size = new System.Drawing.Size(297, 351);
+            this.panel1.TabIndex = 8;
+            // 
+            // iconButton1
+            // 
+            this.iconButton1.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(2)))), ((int)(((byte)(19)))), ((int)(((byte)(87)))));
+            this.iconButton1.Cursor = System.Windows.Forms.Cursors.Hand;
+            this.iconButton1.FlatAppearance.BorderSize = 0;
+            this.iconButton1.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
+            this.iconButton1.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.iconButton1.ForeColor = System.Drawing.Color.WhiteSmoke;
+            this.iconButton1.IconChar = FontAwesome.Sharp.IconChar.MapLocation;
+            this.iconButton1.IconColor = System.Drawing.Color.White;
+            this.iconButton1.IconFont = FontAwesome.Sharp.IconFont.Auto;
+            this.iconButton1.IconSize = 42;
+            this.iconButton1.Location = new System.Drawing.Point(43, 168);
+            this.iconButton1.Margin = new System.Windows.Forms.Padding(2);
+            this.iconButton1.Name = "iconButton1";
+            this.iconButton1.Size = new System.Drawing.Size(208, 60);
+            this.iconButton1.TabIndex = 7;
+            this.iconButton1.Text = "VER RUTA MAS USADA";
+            this.iconButton1.TextImageRelation = System.Windows.Forms.TextImageRelation.ImageBeforeText;
+            this.iconButton1.UseVisualStyleBackColor = false;
+            this.iconButton1.Click += new System.EventHandler(this.iconButton1_Click);
+            // 
+            // cajaferroviaria
+            // 
+            this.cajaferroviaria.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(2)))), ((int)(((byte)(19)))), ((int)(((byte)(87)))));
+            this.cajaferroviaria.Cursor = System.Windows.Forms.Cursors.Hand;
+            this.cajaferroviaria.FlatAppearance.BorderSize = 0;
+            this.cajaferroviaria.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
+            this.cajaferroviaria.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.cajaferroviaria.ForeColor = System.Drawing.Color.WhiteSmoke;
+            this.cajaferroviaria.IconChar = FontAwesome.Sharp.IconChar.MapLocation;
+            this.cajaferroviaria.IconColor = System.Drawing.Color.White;
+            this.cajaferroviaria.IconFont = FontAwesome.Sharp.IconFont.Auto;
+            this.cajaferroviaria.IconSize = 42;
+            this.cajaferroviaria.Location = new System.Drawing.Point(43, 69);
+            this.cajaferroviaria.Margin = new System.Windows.Forms.Padding(2);
+            this.cajaferroviaria.Name = "cajaferroviaria";
+            this.cajaferroviaria.Size = new System.Drawing.Size(208, 60);
+            this.cajaferroviaria.TabIndex = 6;
+            this.cajaferroviaria.Text = "VER POR FECHAS";
+            this.cajaferroviaria.TextImageRelation = System.Windows.Forms.TextImageRelation.ImageBeforeText;
+            this.cajaferroviaria.UseVisualStyleBackColor = false;
+            this.cajaferroviaria.Click += new System.EventHandler(this.cajaferroviaria_Click);
+            // 
+            // iconButton2
+            // 
+            this.iconButton2.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(2)))), ((int)(((byte)(19)))), ((int)(((byte)(87)))));
+            this.iconButton2.Cursor = System.Windows.Forms.Cursors.Hand;
+            this.iconButton2.FlatAppearance.BorderSize = 0;
+            this.iconButton2.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
+            this.iconButton2.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.iconButton2.ForeColor = System.Drawing.Color.WhiteSmoke;
+            this.iconButton2.IconChar = FontAwesome.Sharp.IconChar.RightToBracket;
+            this.iconButton2.IconColor = System.Drawing.Color.White;
+            this.iconButton2.IconFont = FontAwesome.Sharp.IconFont.Auto;
+            this.iconButton2.IconSize = 42;
+            this.iconButton2.Location = new System.Drawing.Point(55, 562);
+            this.iconButton2.Margin = new System.Windows.Forms.Padding(2);
+            this.iconButton2.Name = "iconButton2";
+            this.iconButton2.Size = new System.Drawing.Size(143, 57);
+            this.iconButton2.TabIndex = 15;
+            this.iconButton2.Text = "atras";
+            this.iconButton2.TextImageRelation = System.Windows.Forms.TextImageRelation.ImageBeforeText;
+            this.iconButton2.UseVisualStyleBackColor = false;
+            this.iconButton2.Click += new System.EventHandler(this.iconButton2_Click);
+            // 
+            // label1
+            // 
+            this.label1.BackColor = System.Drawing.Color.Transparent;
+            this.label1.Dock = System.Windows.Forms.DockStyle.Top;
+            this.label1.Font = new System.Drawing.Font("Microsoft Sans Serif", 28.2F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label1.Location = new System.Drawing.Point(0, 0);
+            this.label1.Margin = new System.Windows.Forms.Padding(2, 0, 2, 0);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(684, 78);
+            this.label1.TabIndex = 16;
+            this.label1.Text = "REPORTES";
+            this.label1.TextAlign = System.Drawing.ContentAlignment.MiddleCenter;
+            this.label1.Click += new System.EventHandler(this.label1_Click);
+            // 
+            // VenReportes
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.BackgroundImage = ((System.Drawing.Image)(resources.GetObject("$this.BackgroundImage")));
+            this.BackgroundImageLayout = System.Windows.Forms.ImageLayout.Stretch;
+            this.ClientSize = new System.Drawing.Size(684, 661);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.iconButton2);
+            this.Controls.Add(this.panel1);
+            this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.FixedSingle;
+            this.MinimumSize = new System.Drawing.Size(700, 700);
+            this.Name = "VenReportes";
+            this.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen;
+            this.Text = "VenReportes";
+            this.panel1.ResumeLayout(false);
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Panel panel1;
+        private FontAwesome.Sharp.IconButton iconButton1;
+        private FontAwesome.Sharp.IconButton cajaferroviaria;
+        private FontAwesome.Sharp.IconButton iconButton2;
+        private System.Windows.Forms.Label label1;
+    }
+}
